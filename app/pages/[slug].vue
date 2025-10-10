afdb044d6b9d2a63af4bcf2b2ace72ebde316cbb
Léo Guillaume <leo.guillaume@epitech.eu>
2025-10-11 00:44:08 +0200

    Initial commit

---
+++ app/pages/[slug].vue
@@ -0,0 +1,16 @@
+<script setup>
+const slug = useRoute().params.slug;
+
+const story = await useAsyncStoryblok(
+    slug && slug.length > 0 ? slug.join('/') : 'home',
+    {
+      api: {
+        version: 'draft',
+      },
+    },
+);
+</script>
+
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
+</template>
