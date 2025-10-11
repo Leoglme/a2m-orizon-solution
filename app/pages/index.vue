b2f9d2a4d772c6b33b259b69a63201e1ce7805ea
Léo Guillaume <leo.guillaume@epitech.eu>
2025-10-11 07:10:49 +0200

    feat: tabs component

---
+++ app/pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { story } = await useAsyncStoryblok(
+    '/pages',
+    {
+      bridge: {},
+      api: {
+        version: process.env.NODE_ENV === 'production' ? 'published' : 'draft',
+      },
+    }
+);
+</script>
+
+<template>
+  <StoryblokComponent v-if="story" :blok="story.content" />
+</template>
