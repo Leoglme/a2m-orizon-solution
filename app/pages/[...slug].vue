d651a8e56687dea9c44566cfecaf37b8b00ce640
Léo Guillaume <leo.guillaume@epitech.eu>
2025-10-12 07:13:46 +0200

    feat: add prestation v1 page

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -6,19 +6,20 @@ import type { RouteLocationNormalizedLoadedGeneric } from "#vue-router";
 
 const route: RouteLocationNormalizedLoadedGeneric = useRoute();
 const slug: string | string[] | undefined  = route.params.slug;
+
 const storyPath: ComputedRef<string> = computed(() => {
-  if(!slug) {
-    return 'home';
-  }
-  if(typeof slug === 'string') {
-    return `/pages/${slug}`;
-  }
-  if(Array.isArray(slug) && slug.length > 0) {
-    return `/pages/${slug.join('/')}`;
-  } else {
-    return 'home';
-  }
-});
+  if (!slug) return '/pages'
+
+  const last: string =
+      Array.isArray(slug) && slug.length > 0
+          ? slug[slug.length - 1] || ''
+          : typeof slug === 'string'
+              ? slug
+              : ''
+
+  return `/pages/${last}`
+})
+
 const { story } = await useAsyncStoryblok(
     storyPath.value,
     {
